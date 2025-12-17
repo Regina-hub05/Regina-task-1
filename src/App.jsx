600c951cf140866c0f100c59343588803b51f775
Regina-hub05 <regishka459@gmail.com>
2025-12-17 19:42:32 +0500

    click counter

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,25 @@
 import React, { useState } from 'react';
 
 function App() {  
-  const [number, setNumber] = useState(0); 
+  const [plusCount, setPlusCount] = useState(0);
+  const [minusCount, setMinusCount] = useState(0); 
   
-  const generate = () => {
-    const random = Math.floor(Math.random() * 101); 
-    setNumber(random);
+  const handlePlus = () => {
+    setPlusCount(plusCount + 1);
+  }; 
+  
+  const handleMinus = () => {
+    setMinusCount(minusCount + 1);
   };
   
   return (
-   <div style={{ padding: '30px' }}>
-    
-      <h1>{number}</h1>     
-      
-      <button onClick={generate}>Случайное число</button>
+    <div style={{ padding: '30px' }}>    
+      <div>+ нажали: {plusCount}</div>
+      <div>- нажали: {minusCount}</div>
+       <button onClick={handlePlus} style={{ marginRight: '10px' }}>+</button>
+      <button onClick={handleMinus}>-</button>
     </div>
   );
 }
+
 export default App;
